8049ee07025a6840dd1530157ecf9b61871b43f5
Isaiah Ernest Ovie <officialisaiahovie@gmail.com>
2025-01-28 16:56:54 +0100

    fix: add type safety to the processFileInput function in /lib/utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,13 +5,19 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const processFileInput = (input: unknown) => {
+type FileLikeType =
+  | File
+  | string
+  | { name: string; type: string; content: string };
+
+export const processFileInput = (input: FileLikeType) => {
   if (input instanceof File) return input; // Already a File object
   if (typeof input === 'object' && input?.name && input?.type) {
     // Convert plain object to File
-    return new File([input?.content || ''], input?.name, {
+    const newFile: File = new File([input?.content || ''], input?.name, {
       type: input?.type,
     });
+    return newFile;
   }
   return input; // Return as is for URL validation
 };
